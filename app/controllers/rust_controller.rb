26001ef8409d1bd3576a509d1316bf3785bd18fd
Lluís Gili <lluis@ingent.net>
2017-10-06 19:38:15 +0200

    move pre-wipe data to wipe 0

--- app/controllers/rust_controller.rb
+++ app/controllers/rust_controller.rb
@@ -4,15 +4,23 @@ class RustController < ApplicationController
   layout false
 
   def index
-    @timeline = []
-    @timeline = Player.all.collect {|player|
+    @timeline = Player.where(nil)
+    case params[:wipe]
+    when '0'
+      @timeline = @timeline.where("login < '2017-10-05 18:00:00' or last_seen < '2017-10-05 18:00:00'")
+    else
+      @timeline = @timeline.where("login >= '2017-10-05 18:00:00' or last_seen >= '2017-10-05 18:00:00'")
+    end
+    @timeline = @timeline.all
+    start_tick = @timeline.first.login
+    end_tick   = @timeline.last.last_seen
+
+    @timeline = @timeline.collect {|player|
       [player.name, player.login.to_s, player.last_seen.to_s || Time.now.to_s]
     }
     @timeline = @timeline.sort {|a,b| a[0].casecmp(b[0]) }.collect { |entry|
       "[\"#{entry[0]}\", new Date(\"#{entry[1].gsub(/ UTC/,'Z')}\"), new Date(\"#{entry[2].gsub(/ UTC/,'Z')}\")]"
     }.join(",\n")
-    start_tick = Player.first.login
-    end_tick   = Player.last.last_seen
     diff_ticks = (end_tick - start_tick).to_i / 8.0
     @ticks = "["
     8.times do
